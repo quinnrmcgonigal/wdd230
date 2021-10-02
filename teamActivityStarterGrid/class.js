1096b0b866876bd1cc849b4cafb4c146bf5c635b
quinnrmcgonigal <mcg17005@byui.edu>
2021-10-01 22:47:13 -0600

    leesss goooooo

---
+++ teamActivityStarterGrid/class.js
@@ -0,0 +1,39 @@
+function showAlert() {
+    var myText = "You've signed in!";
+    alert (myText);
+  }
+
+  color = '#FFCF2D'
+document.querySelector('.popular').style.backgroundColor = color;
+
+y = document.getElementsByTagName('a');
+for (let i = 0; i < y.length; i++) {
+    y[i].style.backgroundColor = '#FFCF2D';
+}
+
+
+
+//appear
+  function showDiv1() {
+    setTimeout(function () {
+        document.getElementById("card1").style.visibility = "visible";
+    }, 1000);
+}
+
+showDiv1();
+
+function showDiv2() {
+  setTimeout(function () {
+      document.getElementById("card2").style.visibility = "visible";
+  }, 2000);
+}
+
+showDiv2();
+
+function showDiv3() {
+  setTimeout(function () {
+      document.getElementById("card3").style.visibility = "visible";
+  }, 3000);
+}
+
+showDiv3();
